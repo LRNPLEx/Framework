935012fd409c480eff271defde9b737b9beaa9e8
David Kluszczynski <dave07747@gmail.com>
2016-06-23 19:25:39 -0400

    Started Accel Handler, but leaving for vaca, so not done yet

---
+++ app/src/main/java/com/learnplex/framework/impl/AccelerometerHandler.java
@@ -0,0 +1,14 @@
+package com.learnplex.framework.impl;
+
+import android.hardware.SensorEventListener;
+
+/**
+ * Created by David on 6/23/2016.
+ */
+public class AccelerometerHandler implements SensorEventListener {
+    float accelX;
+    float accelY;
+    float accelZ;
+
+
+}
