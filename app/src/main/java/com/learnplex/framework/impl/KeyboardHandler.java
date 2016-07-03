6662c0fe11b9c735ab831d8398e0653659048b31
David Kluszczynski <dave07747@gmail.com>
2016-07-02 20:21:50 -0400

    Keyboard Handler - finished

--- app/src/main/java/com/learnplex/framework/impl/KeyboardHandler.java
+++ app/src/main/java/com/learnplex/framework/impl/KeyboardHandler.java
@@ -15,44 +15,63 @@ import java.util.List;
  */
 public class KeyboardHandler implements View.OnKeyListener {
     boolean[] pressedKeys = new boolean[128];
-    Pool<Input.KeyEvent>keyEventPool;
-    List<Input.KeyEvent>keyEventsBuffer = new ArrayList<Input.KeyEvent>();
-    List<Input.KeyEvent>keyEvents = new ArrayList<Input.KeyEvent>();
+    Pool<Input.KeyEvent> keyEventPool;
+    List<Input.KeyEvent> keyEventsBuffer = new ArrayList<Input.KeyEvent>();
+    List<Input.KeyEvent> keyEvents = new ArrayList<Input.KeyEvent>();
 
-    public KeyboardHandler(View view){
-        Pool.PoolObjectFactory<Input.KeyEvent>factory = new Pool.PoolObjectFactory<Input.KeyEvent>() {
+    public KeyboardHandler(View view) {
+        Pool.PoolObjectFactory<Input.KeyEvent> factory = new Pool.PoolObjectFactory<Input.KeyEvent>() {
             @Override
             public Input.KeyEvent createObject() {
                 return new Input.KeyEvent();
             }
         };
-    keyEventPool = new Pool<Input.KeyEvent>(factory, 100);
-    view.setOnKeyListener(this);
-    view.setFocusableInTouchMode(true);
-    view.requestFocus();
+        keyEventPool = new Pool<Input.KeyEvent>(factory, 100);
+        view.setOnKeyListener(this);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
     }
 
     @Override
     public boolean onKey(View view, int keyCode, KeyEvent event) {
-        if(event.getAction() == android.view.KeyEvent.ACTION_MULTIPLE)
+        if (event.getAction() == android.view.KeyEvent.ACTION_MULTIPLE)
             return false;
 
-        synchronized (this){
+        synchronized (this) {
             Input.KeyEvent keyEvent = keyEventPool.newObject();
             keyEvent.keyCode = keyCode;
             keyEvent.keyChar = event.getUnicodeChar();
-            if(event.getAction() == KeyEvent.ACTION_DOWN){
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
                 keyEvent.type = Input.KeyEvent.KEY_DOWN;
-                if(keyCode > 0 && keyCode < 127)
+                if (keyCode > 0 && keyCode < 127)
                     pressedKeys[keyCode] = true;
             }
-            if(event.getAction() == KeyEvent.ACTION_UP){
+            if (event.getAction() == KeyEvent.ACTION_UP) {
                 keyEvent.type = Input.KeyEvent.KEY_UP;
-                if(keyCode > 0 && keyCode < 127)
+                if (keyCode > 0 && keyCode < 127)
                     pressedKeys[keyCode] = false;
             }
             keyEventsBuffer.add(keyEvent);
         }
         return false;
+    }
+
+    public boolean isKeyPressed(int keyCode) {
+        if (keyCode < 0 || keyCode > 127)
+            return false;
+        return pressedKeys[keyCode];
+    }
+
+    public List<Input.KeyEvent> getKeyEvents() {
+        synchronized (this) {
+            int len = keyEvents.size();
+            for (int i = 0; i < len; ++i) {
+                keyEventPool.free(keyEvents.get(i));
+            }
+            keyEvents.clear();
+            keyEvents.addAll(keyEventsBuffer);
+            keyEventsBuffer.clear();
+            return keyEvents;
+        }
     }
 }
